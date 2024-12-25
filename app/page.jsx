9441996e8c0dd3c9c7b26b242c330659b95a45b9
Deyvid Kasteh <kasteh21@gmail.com>
2024-12-25 18:43:42 -0300

    feat: Home page improvement

--- app/page.jsx
+++ app/page.jsx
@@ -1,3 +1,37 @@
-export default function Home() {
-  return <main>Homepage</main>;
-}
+import { Button } from "@/components/ui/button";
+import { FiDownload } from "react-icons/fi";
+
+const Home = () => {
+  return (
+    <section className="h-full">
+      <div className="container mx-auto h-full">
+        <div className="flex flex-col xl:flex-row items-center justify-between xl:tp-8 xl:pb-24">
+          <div className="text-center xl:text-left">
+            <span className="text-xl">Software Developer</span>
+            <h1>
+              Hello I'm <br />
+              <span>Deyvid Kasteh</span>
+            </h1>
+            <p className="max-w-[500px] mb-9 text-white">
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores
+              quis ut sit similique, molestiae deserunt sint architecto natus
+              perferendis quidem obcaecati, soluta porro reprehenderit! Magnam
+              inventore saepe qui quaerat optio?
+            </p>
+            <div className="flex flex-col xl:flex-row items-center gap-8">
+              <Button variant="secondary">
+                <span>Download CV</span>
+                <FiDownload/>
+              </Button>
+              <div className="mb-8 xl:mb-0">Social</div>
+            </div>
+          </div>
+          <div>photo</div>
+        </div>
+      </div>
+      oi
+    </section>
+  );
+};
+
+export default Home;
