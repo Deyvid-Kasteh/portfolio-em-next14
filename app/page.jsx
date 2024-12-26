36b3044ebf8e3148ab8916471972bbd8557b7972
Deyvid Kasteh <kasteh21@gmail.com>
2024-12-25 22:49:28 -0300

    feat: Social componente implementation on homepage

--- app/page.jsx
+++ app/page.jsx
@@ -1,6 +1,11 @@
 import { Button } from "@/components/ui/button";
 import { FiDownload } from "react-icons/fi";
 
+
+
+// components
+import Social from "@/components/Social";
+
 const Home = () => {
   return (
     <section className="h-full">
@@ -23,13 +28,14 @@ const Home = () => {
                 <span>Download CV</span>
                 <FiDownload/>
               </Button>
-              <div className="mb-8 xl:mb-0">Social</div>
+              <div className="mb-8 xl:mb-0">
+                <Social containerStyles="flex gap-6" iconStyles="text-2xl w-9 h-9 border border-accent rounded-full flex justify-center items-center text-accent text-base hover:bg-accent hover:text-primary hover:transition-all duration-500" />
+              </div>
             </div>
           </div>
           <div>photo</div>
         </div>
       </div>
-      oi
     </section>
   );
 };
