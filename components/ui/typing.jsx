b75354c6b4b1f6a0de682170cdb8952fed4bd187
Deyvid Kasteh <kasteh21@gmail.com>
2024-12-30 00:42:01 -0300

    feat: 3 dots (typing) component addition 2

--- components/ui/typing.jsx
+++ components/ui/typing.jsx
@@ -7,9 +7,14 @@ const Typing = () => {
   return (
     <div className="flex justify-center items-center">
       <svg height="40" width="40" className="loader">
+        <circle className="dot fill-yellow-400" cx="10" cy="10" r="3" />
         <circle className="dot fill-yellow-400" cx="10" cy="20" r="3" />
-        <circle className="dot fill-yellow-400" cx="20" cy="20" r="3" />
-        <circle className="dot fill-yellow-400" cx="30" cy="20" r="3" />
+        <circle className="dot fill-yellow-400" cx="10" cy="30" r="3" />
+
+        <circle className="dot2 fill-yellow-400" cx="20" cy="15" r="3" />
+        <circle className="dot2 fill-yellow-400" cx="20" cy="25" r="3" />
+
+        <circle className="dot3 fill-yellow-400" cx="30" cy="20" r="3" />
       </svg>
     </div>
   );
